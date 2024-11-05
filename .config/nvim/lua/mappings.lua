f56955486d079a29ec71da505d03e1a8642547b5
Dreadyyyy <aboutegor8@gmail.com>
2024-11-06 00:17:04 +0300

    Neovim transparent on startup and keybind to toggle transparency

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,3 +9,4 @@ map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 map("n", "<Leader>lf", vim.diagnostic.open_float, { desc = "Show diagnostic under cursor" })
 map("n", "<Leader>q", vim.diagnostic.setloclist, { desc = "Show diagnostic locklist" })
+map("n", "<Leader>tt", require("base46").toggle_transparency, { desc = "Toggle transparency" })
